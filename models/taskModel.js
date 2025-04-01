9c106d11093559366952baa3efcb68ee9ea78f16
mislangc <carlbpmislang@gmail.com>
2025-04-01 10:27:06 +0800

    Revert "task model, controller, route added. (not yet tested)"
    This reverts commit 6faee55b38fc201bf13eed8bf83035dfb6b37842.

--- models/taskModel.js
+++
@@ -1,8 +0,0 @@
-const connectDB = require("../db/connect");
-require("dotenv").config();
-
-const taskModel = async () => {
-    return await connectDB(process.env.MONGO_URI).collection('tasks');
-};
-
-module.exports = taskModel;
