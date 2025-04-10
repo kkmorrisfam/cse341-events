03977c9c856d7276ff584a0bebe1e09994e40ccb
CenterValentine <david@doxy.me>
2025-04-09 21:21:11 -0600

    ...

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const eventController = require('../controllers/eventController');
 const {createValidationRules, validation} = require('../utils/eventValidations');
-//const validation = require('../middleware/validate');
+// const validation = require('../middleware/validate');
 //const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', eventController.getAll);
