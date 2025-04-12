f60ad7117b3c51a36f145e8bf152c13660c6a5ec
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-04-12 11:55:11 -0700

    add getAllUsers test

--- __tests__/userGet.test.js
+++ __tests__/userGet.test.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel");
 const TestResponse = require("../utils/test-response");
-const { getUser } = require("../controllers/userController");
+const { getUser, getAllUsers } = require("../controllers/userController");
 const {
   Types: { ObjectId },
 } = require("mongoose");
@@ -36,5 +36,51 @@ describe("Test Get User", () => {
     console.log("User:", JSON.stringify(_user, null, 2));
 
     expect(res.data.toJSON()).toEqual(_user);
+  });
+});
+
+describe("Test Get All Users", () => {
+  test("Returns all users", async () => {
+    const _users = [
+      {
+        _id: new ObjectId("67faa6b71e85afe442d1481e"),
+        username: "FredFlinstone",
+        firstname: "Fred",
+        lastname: "Flinstone",
+        email: "any@email.com",
+        phone: "800-456-7891",
+      },
+      {
+        _id: new ObjectId("67f6e6786be8af49cfb8b288"),
+        username: "BarneyRubble",
+        firstname: "Barney",
+        lastname: "Rubble",
+        email: "barney@gmail.com",
+        phone: "702-123-4567",
+      },
+    ];
+
+    mockingoose(User).toReturn(_users, "find");
+
+    const request = {};
+    const response = new TestResponse();
+    const next = jest.fn();
+
+    await getAllUsers(request, response, next);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.data.length).toBe(2);
+    expect(response.data[0]._id.toString()).toBe("67faa6b71e85afe442d1481e");
+    expect(response.data[0].username).toBe("FredFlinstone");
+    expect(response.data[0].firstname).toBe("Fred");
+    expect(response.data[0].lastname).toBe("Flinstone");
+    expect(response.data[0].email).toBe("any@email.com");
+    expect(response.data[0].phone).toBe("800-456-7891");
+    expect(response.data[1]._id.toString()).toBe("67f6e6786be8af49cfb8b288");
+    expect(response.data[1].username).toBe("BarneyRubble");
+    expect(response.data[1].firstname).toBe("Barney");
+    expect(response.data[1].lastname).toBe("Rubble");
+    expect(response.data[1].email).toBe("barney@gmail.com");
+    expect(response.data[1].phone).toBe("702-123-4567");
   });
 });
