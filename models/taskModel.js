3ed3d462c527a53e084a792efe57581804adccf9
mislangc <carlbpmislang@gmail.com>
2025-04-13 01:07:56 +0800

    tasks: unit testing work in progress

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,40 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-/*const taskModel = async () => {
-    return await connectDB(process.env.MONGO_URI).collection('tasks');
-};*/
-
 const taskSchema = new Schema({
     taskName: String,
     taskDescription: String,
     eventId: String,
     organizerId: String,
     dueDate: String,
-    completed: Boolean    /*taskName: {
-        type: String,
-        required: [true, "taskName is required."],
-        minLength: [4, "taskName must be at least 4 characters."]
-    },
-    taskDescription: {
-        type: String
-    },
-    eventId: {
-        type: String,
-        required: [true, "eventId is required."]
-    },
-    organizerId: {
-        type: String,
-        required: [true, "organizerId is required."]
-    },
-    dueDate: {
-        type: String,
-        required: [true, "specifying the due date is required."]
-    },
-    completed: {
-        type: Boolean,
-        required: [true, "true or false completed is required."],
-    },*/
+    completed: Boolean
 });
 
 const Task = mongoose.model("task", taskSchema);
