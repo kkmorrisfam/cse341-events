a7f1aa1f70752c1fc0612367d02ea79be0984afd
Claudia Madrid <71229725+Blueclaus13@users.noreply.github.com>
2025-04-12 14:49:30 -0600

    added dependency

--- __tests__/eventGet.test.js
+++ __tests__/eventGet.test.js
@@ -1,6 +1,23 @@
+
+jest.mock('passport', () => ({
+    use: jest.fn(),
+    authenticate: () => (req, res, next) => next(),
+    initialize: () => (req, res, next) => next(),
+    session: () => (req, res, next) => next(),
+    serializeUser: jest.fn(),
+    deserializeUser: jest.fn(),
+  }));
+
+jest.mock('passport-google-oauth2', () => ({
+    Strategy: jest.fn().mockImplementation(() => ({}))
+  }));
+
 const {Types: {ObjectId}} = require('mongoose');
 const Event = require("../models/eventModel");
 const {getSingle} = require('../controllers/eventController');
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
 const mockingoose = require('mockingoose');
 const TestResponse= require('../utils/test-response');
 
@@ -24,7 +41,6 @@ describe("Test Get Event", () => {
         const req = {
             params: { id: '67f650618aeb24ff8dbb3337'}
         };
-
         const res = new TestResponse();
         const next = jest.fn(); 
         
@@ -34,5 +50,12 @@ describe("Test Get Event", () => {
          console.log("Event:", JSON.stringify(_event, null, 2));
        
         expect(res.data.toJSON()).toEqual(_event);
+    });
+
+    test('GET/events should return all events', async () => {
+        const res = await request.get('/events');
+        expect(res.statusCode).toBe(200);
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(Array.isArray(res.body)).toBe(true);
     });
   });
