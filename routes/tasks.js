55075639d15d54afe2fb8bf12a2df42b17ff1831
mislangc <carlbpmislang@gmail.com>
2025-04-13 11:02:00 +0800

    tasks: getOneTask unit test done

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const taskController = require('../controllers/taskController');
 const { validateTask, validateObjectId } = require('../utils/taskValidation');
-//const { isAuthenticated } = require('../utils/authenticate');
+const isAuthenticated = require('../utils/isAuthenticated');
 
 router.get('/', taskController.getAllTasks);
 router.get('/:id', validateObjectId, taskController.getOneTask);
-router.post('/', validateTask, taskController.createTask);
-router.put('/:id', validateObjectId, validateTask, taskController.updateTask);
-router.delete('/:id', validateObjectId, taskController.deleteTask);
-router.delete('/taskName/:name', taskController.deleteManyTasks);
+router.post('/', isAuthenticated, validateTask, taskController.createTask);
+router.put('/:id', isAuthenticated, validateObjectId, validateTask, taskController.updateTask);
+router.delete('/:id', isAuthenticated, validateObjectId, taskController.deleteTask);
+router.delete('/taskName/:name', isAuthenticated, taskController.deleteManyTasks);
 
 module.exports = router;
